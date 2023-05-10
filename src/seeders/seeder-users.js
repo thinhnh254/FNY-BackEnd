c4c34251806f6d51042c5f694560fdacc3762881
Thinh <nguyenhungthinh2504@gmail.com>
2023-05-10 16:55:57 +0700

    create Models,Migrations, Seeders

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "example@gmail.com",
+        password: "123456",
+        firstName: "Thinh",
+        lastName: "Nguyen",
+        address: "VNI",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
