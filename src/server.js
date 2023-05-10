9c2dec78fe7f6fc8c05286536b853c798d4ec829
Thinh <nguyenhungthinh2504@gmail.com>
2023-05-10 09:57:27 +0700

    connect MySql DB with Sequelize and render data to view

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import bodyParser from "body-parser";
 import express from "express";
+import connectDB from "./config/connectDB";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-require('dotenv').config();
-
+require("dotenv").config();
 
 let app = express();
 
@@ -15,10 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
+
 let port = process.env.PORT || 2504;
 // port === undefined => port = 2504
 
 app.listen(port, () => {
   //callback
-  console.log("BE NodeJs is running ons port: " + port);
+  console.log("BE NodeJs is running on port: " + port);
 });
