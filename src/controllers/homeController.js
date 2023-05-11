6cf099137a15d42a06c6a1719a50c66dcd3935a7
Thinh <nguyenhungthinh2504@gmail.com>
2023-05-12 00:36:11 +0700

    CRUD (Create)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDservice from "../services/CRUDservice";
 
 let getHomePage = async (req, res) => {
   try {
@@ -18,7 +19,19 @@ let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
 
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDservice.createNewUser(req.body);
+  console.log(message);
+  return res.send("post CRUD from server");
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
