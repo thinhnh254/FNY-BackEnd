3dc55871e26842e18720940c91f35857f3e41b6b
Thinh <nguyenhungthinh2504@gmail.com>
2023-05-12 12:12:07 +0700

    CRUD (Read)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -29,9 +29,20 @@ let postCRUD = async (req, res) => {
   return res.send("post CRUD from server");
 };
 
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDservice.getAllUser();
+  console.log("===========================");
+  console.log(data);
+  console.log("===========================");
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
 };
