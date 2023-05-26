e5e809f0d1b0cf4caf12683416a24a956af18744
Thinh <nguyenhungthinh2504@gmail.com>
2023-05-27 01:30:57 +0700

    REST api

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
@@ -21,6 +22,8 @@ let initWebRoutes = (app) => {
   router.get("/delete-crud", homeController.deleteCRUD);
 
   //rest api
+
+  router.post("/api/login", userController.handleLogin);
 
   return app.use("/", router);
 };
---
+++ src/services/userService.js
@@ -0,0 +1,64 @@
+import bcrypt from "bcrypt";
+import db from "../models/index";
+
+let handleUserLogin = (email, password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userData = {};
+
+      let isExist = await checkUserEmail(email);
+      if (isExist) {
+        let user = await db.User.findOne({
+          where: { email: email },
+          attributes: ["email", "roleId", "password"],
+          raw: true,
+        });
+
+        if (user) {
+          let check = await bcrypt.compareSync(password, user.password);
+
+          if (check) {
+            userData.errCode = 0;
+            userData.errMessage = "Ok";
+            delete user.password;
+            userData.user = user;
+          } else {
+            userData.errCode = 3;
+            userData.errMessage = "!Password";
+          }
+        } else {
+          userData.errCode = 2;
+          userData.errMessage = "User's not found!";
+        }
+      } else {
+        userData.errCode = 1;
+        userData.errMessage = "Email does not exist";
+      }
+      resolve(userData);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let checkUserEmail = (userEmail) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { email: userEmail },
+      });
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  handleUserLogin: handleUserLogin,
+  checkUserEmail: checkUserEmail,
+};
