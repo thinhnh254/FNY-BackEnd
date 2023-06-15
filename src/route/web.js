b670422f1ba01559d4ef2218a64e7dcf60efb981
Thinh <nguyenhungthinh2504@gmail.com>
2023-06-15 09:43:55 +0700

    READ (CRUD)

--- src/route/web.js
+++ src/route/web.js
@@ -25,7 +25,7 @@ let initWebRoutes = (app) => {
 
   router.post("/api/login", userController.handleLogin);
 
-  router.get("/api/get-all-user", userController.handleGetAllUsers);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
 
   return app.use("/", router);
 };
